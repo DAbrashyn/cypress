1304456501bf11b540c13b70255070a4c5a04e32
unknown <denis.abrashin@gmail.com>
2026-02-02 15:24:11 +0200

    removed old files/added several config files

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,14 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  projectId: 'jztgsx',
-  e2e: {
-    baseUrl: "https://demoqa.com/webtables",
-    defaultCommandTimeout: 10000,
-    viewportHeight: 900,
-    viewportWidth: 1440,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+	projectId: 'jztgsx',
+	e2e: {
+		baseUrl: 'https://demoqa.com/webtables',
+		defaultCommandTimeout: 10000,
+		viewportHeight: 900,
+		viewportWidth: 1440,
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
 });
--- cypress/e2e/homework19.cy.js
+++ cypress/e2e/homework19.cy.js
@@ -1,41 +1,43 @@
 describe('HW 19.1. Cypress queries', () => {
+	const user = Cypress.env('basicAuthUser');
+	const pass = Cypress.env('basicAuthPass');
+	const url = 'qauto.forstudy.space';
+	const signUp = '.hero-descriptor_btn';
 
-const user = Cypress.env('basicAuthUser');
-const pass = Cypress.env('basicAuthPass');
-const url = 'qauto.forstudy.space';
-const signUp = '.hero-descriptor_btn'
-  
-  beforeEach(() => {   
-    cy.visit(`https://${user}:${pass}@${url}`);
-  });
+	beforeEach(() => {
+		cy.visit(`https://${user}:${pass}@${url}`);
+	});
 
-  it('Check Logo, Home, About, Contacts, Guest Log in, Sign In, Sign Up', () => {
-    //section hero
-    cy.get('.header').within(() => {      
-      cy.get('.header_logo').should('be.visible');
-      cy.contains('Home').should('be.visible');
-      cy.contains('About').should('be.visible');
-      cy.contains('Contacts').should('be.visible');
-      cy.get('.header-link.-guest').should('be.visible');
-      cy.get('.btn-outline-white').should('be.visible');      
-    });
-  });
+	it('Check Logo, Home, About, Contacts, Guest Log in, Sign In, Sign Up', () => {
+		//section hero
+		cy.get('.header').within(() => {
+			cy.get('.header_logo').should('be.visible');
+			cy.contains('Home').should('be.visible');
+			cy.contains('About').should('be.visible');
+			cy.contains('Contacts').should('be.visible');
+			cy.get('.header-link.-guest').should('be.visible');
+			cy.get('.btn-outline-white').should('be.visible');
+		});
+	});
 
-  it('Check Do more!', () => {
-     cy.contains('.hero-descriptor_title', 'Do more!').should('be.visible');
-     cy.contains('.hero-descriptor_descr', 'With the help of the Hillel auto project, you will have the opportunity to get hands-on experience in manual testing.').should('be.visible');
-     cy.contains('.hero-descriptor_btn', 'Sign up').should('be.visible');     
-  });
+	it('Check Do more!', () => {
+		cy.contains('.hero-descriptor_title', 'Do more!').should('be.visible');
+		cy.contains(
+			'.hero-descriptor_descr',
+			'With the help of the Hillel auto project, you will have the opportunity to get hands-on experience in manual testing.',
+		).should('be.visible');
+		cy.contains('.hero-descriptor_btn', 'Sign up').should('be.visible');
+	});
 
-  it('Check container of Contacts', () => {
-    cy.get('.section.contacts').within(() => {      
-      cy.get('.icon-facebook').should('be.visible');
-      cy.get('.icon-telegram').should('be.visible');
-      cy.get('.icon-youtube').should('be.visible');
-      cy.get('.icon-instagram').should('be.visible');
-      cy.get('.icon-linkedin').should('be.visible');       
-      cy.get('a[href="https://ithillel.ua"]').should('be.visible');    
-      cy.get('a[href="mailto:developer@ithillel.ua"]').should('be.visible');
-    });
-  });
+	it('Check container of Contacts', () => {
+		cy.get('.section.contacts').within(() => {
+			cy.get('.icon-facebook').should('be.visible');
+			cy.get('.icon-telegram').should('be.visible');
+			cy.get('.icon-youtube').should('be.visible');
+			cy.get('.icon-instagram').should('be.visible');
+			cy.get('.icon-linkedin').should('be.visible');
+			cy.get('a[href="https://ithillel.ua"]').should('be.visible');
+			cy.get('a[href="mailto:developer@ithillel.ua"]').should('be.visible');
+		});
+	});
 });
